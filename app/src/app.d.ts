2b5f269441cbd50539937310ae6edb74d6d1abd6
Lucas Pruvost <lucas.pruvost@protonmail.com>
2022-07-03 02:07:46 +0200

    Fix missing locals TS interface

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -3,7 +3,10 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		userid: string;
+	}
+
 	// interface Platform {}
 	// interface Session {}
 	// interface Stuff {}
